38672128a15c99b0874bc482dedf3303074f845a
Clement Chan <cchan00@stuy.edu>
2019-12-13 08:40:06 -0500

    add comments

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -1,34 +1,38 @@
+//Clement Chan & Kelvin Ng
+//SoftDev1 pd1
+//K29 -- Sequential Progression III: Season of the Witch
+//2019-12-13
 
+
+//replaces heading with the content of the element
 var changeHeading = function(e){
     var h = document.getElementById("h");
-    var text = this.innerHTM
+    var text = this.innerHTML
     h.innerHTML=text;
 }
 
 var removeItem = function(e){
     this.parentNode.removeChild(this)
-    document.getElementById("h").innerHTML = "Hello World!"
+    document.getElementById("h").innerHTML = "Hello World!" //resets heading
 }
-
-
 
 var lis = document.getElementsByTagName("li")
 for (var i = 0; i<lis.length; i++){
     lis[i].addEventListener('mouseover', changeHeading);
     lis[i].addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World!"});
+    //changes heading back to Hello World!
     lis[i].addEventListener('click', removeItem)
 }
-
-
 
 var addItem = function(e){
     var list = document.getElementById("thelist")
     var item = document.createElement("li")
     item.innerHTML = "WORD";
+    list.appendChild(item);
+    //add same event listeners to added element
     item.addEventListener('mouseover', changeHeading);
     item.addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World!"});
     item.addEventListener('click', removeItem)
-    list.appendChild(item);
 }
 
 
@@ -47,8 +51,8 @@ var fib = function(n){
 
 var addFib = function(e){
     var list = document.getElementById("fiblist")
-    var n = list.childElementCount;
     var item = document.createElement("li")
+    var n = list.childElementCount;//finds num of fib numbers already displayed
     item.innerHTML = fib(n)
     list.appendChild(item)
 }
